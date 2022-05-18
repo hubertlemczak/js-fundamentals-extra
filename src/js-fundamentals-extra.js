ce57af696c7608c4c5d397cf00b6e91489cbe5d1
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-18 16:37:52 +0100

    c

--- src/js-fundamentals-extra.js
+++ src/js-fundamentals-extra.js
@@ -1,14 +1,18 @@
 // SecondsInHours
 //
-// Create a function that takes a number of hours and returns the number of 
-// seconds in those hours. 
+// Create a function that takes a number of hours and returns the number of
+// seconds in those hours.
 //
 // Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
+const hoursToSecondsMultiplier = 3600;
+// function hoursToSeconds(hours) {
+//   return hours * hoursToSecondsMultiplier;
+// }
 
-
+let hoursToSeconds = (hours) => hours * hoursToSecondsMultiplier;
 
 // MilesTravelled
 //
@@ -18,42 +22,46 @@
 // perform the rounding. You can assume only whole numbers will be provided
 // to the function.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
+let distance = (speed, time) => Math.ceil(speed * (time / 60));
 
 // KilometersToMiles
 //
-// Create a function that takes a number of kilometers and converts it to miles. 
-// For this task assume there are exactly 1.6 kilometers in a mile. The returned 
+// Create a function that takes a number of kilometers and converts it to miles.
+// For this task assume there are exactly 1.6 kilometers in a mile. The returned
 // mile distance should be rounded to the nearest mile. You can  use the Math.round
 // function to do this.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
+const kmToMMultiplier = 1.6;
+let kmToM = (km) => Math.round(km / kmToMMultiplier);
 
 // MakeSentence
 //
-// Create a function that takes a string and returns the same string with the 
-// first letter capitalized and a full stop added on to the end. If the string 
-// already ends with a full stop, question mark (?) or an exclamation mark(!) 
+// Create a function that takes a string and returns the same string with the
+// first letter capitalized and a full stop added on to the end. If the string
+// already ends with a full stop, question mark (?) or an exclamation mark(!)
 // then a full stop should not be added.
 //
 // You may need to the use `String.substring` method.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
+let makeSentence = (words) => {
+  let capitaliseFirst = words[0].toUpperCase() + words.slice(1);
+  let endPunctuation = words[words.length - 1].match(/[?!.]/gi);
+  return endPunctuation === null ? `${capitaliseFirst}.` : `${capitaliseFirst}`;
+};
 
 // FileExtension
 //
@@ -62,73 +70,65 @@
 // string should be returned. You may need to the use `String.substring` method and
 // the `String.lastIndexOf` method.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
 
 // Range
 //
-// Create a function that takes an array of numbers and returns the difference 
+// Create a function that takes an array of numbers and returns the difference
 // between the highest and lowest numbers in the array. You can assume the array
 // will always contain at least 1 number (it will never be empty)
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
-
 // CheckTransactions
 //
-// Create a function that checks if a list of banking transactions is valid for 
-// a given account. 
+// Create a function that checks if a list of banking transactions is valid for
+// a given account.
 //
-// The function should accept 3 arguments. The first is an array containing a 
-// list of transactions to apply to the account balance. Positive numbers are 
-// deposits and negative numbers are withdrawals. The second argument is the 
+// The function should accept 3 arguments. The first is an array containing a
+// list of transactions to apply to the account balance. Positive numbers are
+// deposits and negative numbers are withdrawals. The second argument is the
 // account starting balance and the third is the account overdraft. The function
 // should return false if applying the list of transactions causes the account
 // balance to go below the overdraft value at any point.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
-
-
 
 // FilmsInGenre
 //
 // Create a function that takes an array of film objects, a film genre, and returns
-// an array of film names that match the supplied genre. Each film object contains a 
-// `name` key that contains the film name and a 'genre' key that contains an array 
+// an array of film names that match the supplied genre. Each film object contains a
+// `name` key that contains the film name and a 'genre' key that contains an array
 // of strings describing what genres the film belongs to. See the spec file for
 // example data.
 //
-// Remember to update the module.exports definition at the bottom of the file 
+// Remember to update the module.exports definition at the bottom of the file
 // with a reference to your function.
 //
 // TODO: write code below
 
-
-
 // TODO: change undefined to be the name of the functions you defined
 module.exports = {
   //SecondsInHours
-  a: undefined,
+  a: hoursToSeconds,
 
   //MilesTravelled,
-  b: undefined,  
+  b: distance,
 
   //KilometersToMiles,
-  c: undefined, 
+  c: kmToM,
 
   //MakeSentence
-  d: undefined, 
+  d: makeSentence,
 
   //FileExtension
   e: undefined,
@@ -141,4 +141,4 @@ module.exports = {
 
   //FilmsInGenre
   h: undefined,
-}
+};
